79f61dbcde5f106c556baa3e9e345cacd7328985
Leal Kristofferson <USLEALK@tetrapak.com>
2023-01-26 23:09:25 -0600

    Connected front end to back end.

--- src/App.js
+++ src/App.js
@@ -71,6 +71,7 @@ class App extends Component {
     } else if (route === "home") {
       this.setState({ isSignedIn: true });
     }
+    console.log(route);
     this.setState({ route: route });
   };
 
